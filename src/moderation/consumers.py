5c54095dff1daa99114ac101dd0b1cbec2f59a8a
akilo <sergeencowgame@yande.ru>
2025-08-20 20:38:45 +0300

    звонки и тд

--- src/moderation/consumers.py
+++ src/moderation/consumers.py
@@ -1,18 +1,19 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 from .models import CallSession
-from asgiref.sync import sync_to_async
 
 class CallConsumer(WebsocketConsumer):
     def connect(self):
         self.call_id = self.scope['url_route']['kwargs']['call_id']
         self.room_group_name = f'call_{self.call_id}'
 
-        return self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        return self.accept()
+        # Присоединяемся к группе
+        self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        self.accept()
 
     def disconnect(self, close_code):
-        return self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        # Покидаем группу
+        self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     def receive(self, text_data):
         data = json.loads(text_data)
@@ -20,10 +21,10 @@ class CallConsumer(WebsocketConsumer):
 
         if message_type == 'hangup':
             # Обработка завершения звонка
-            return self.channel_layer.group_send(
+            self.channel_layer.group_send(
                 self.room_group_name,
                 {
-                    'type': '',
+                    'type': 'call_hangup',  # вызывается метод send_call_hangup
                     'message': {
                         'type': 'hangup',
                         'sender': data.get('sender'),
@@ -32,14 +33,18 @@ class CallConsumer(WebsocketConsumer):
             )
         else:
             # Обработка других сигналов
-            return self.channel_layer.group_send(
+            self.channel_layer.group_send(
                 self.room_group_name,
                 {
-                    'type': 'send_message',
+                    'type': 'send_message',  # вызывается метод send_message
                     'message': data,
                 }
             )
 
     def send_message(self, event):
-        # Унифицированный метод отправки сообщений
-        return self.send(text_data=json.dumps(event['message']))
+        # Отправляем сообщение клиенту
+        self.send(text_data=json.dumps(event['message']))
+
+    def call_hangup(self, event):
+        # Обработка события hangup
+        self.send(text_data=json.dumps(event['message']))
