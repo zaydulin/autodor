11d80f5d5701ce35a8b67068255d33ec1d4966b4
akilo <sergeencowgame@yande.ru>
2025-08-20 20:40:23 +0300

    звонки и тд

--- src/moderation/routing.py
+++ src/moderation/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/call/(?P<call_id>[0-9a-f\-]+)/$", consumers.CallConsumer.as_asgi()),
+    re_path(r"ws/call/(?P<call_id>\d+)/$", consumers.CallConsumer.as_asgi()),
 ]
