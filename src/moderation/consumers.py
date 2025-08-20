e11d40d894331e1f09ab39ed948f68b9349e9106
akilo <sergeencowgame@yande.ru>
2025-08-20 20:46:51 +0300

    звонки и тд

--- src/moderation/consumers.py
+++ src/moderation/consumers.py
@@ -1,30 +1,41 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 from .models import CallSession
 
-class CallConsumer(WebsocketConsumer):
-    def connect(self):
+class CallConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.call_id = self.scope['url_route']['kwargs']['call_id']
         self.room_group_name = f'call_{self.call_id}'
 
-        # Присоединяемся к группе
-        self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        self.accept()
+        # Проверяем существование сессии звонка
+        if await self.call_session_exists():
+            # Присоединяемся к группе
+            await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name
+            )
+            await self.accept()
+        else:
+            await self.close()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Покидаем группу
-        self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         data = json.loads(text_data)
         message_type = data.get('type')
 
         if message_type == 'hangup':
             # Обработка завершения звонка
-            self.channel_layer.group_send(
+            await self.channel_layer.group_send(
                 self.room_group_name,
                 {
-                    'type': 'call_hangup',  # вызывается метод send_call_hangup
+                    'type': 'call_hangup',
                     'message': {
                         'type': 'hangup',
                         'sender': data.get('sender'),
@@ -33,18 +44,22 @@ class CallConsumer(WebsocketConsumer):
             )
         else:
             # Обработка других сигналов
-            self.channel_layer.group_send(
+            await self.channel_layer.group_send(
                 self.room_group_name,
                 {
-                    'type': 'send_message',  # вызывается метод send_message
+                    'type': 'send_message',
                     'message': data,
                 }
             )
 
-    def send_message(self, event):
+    async def send_message(self, event):
         # Отправляем сообщение клиенту
-        self.send(text_data=json.dumps(event['message']))
+        await self.send(text_data=json.dumps(event['message']))
 
-    def call_hangup(self, event):
+    async def call_hangup(self, event):
         # Обработка события hangup
-        self.send(text_data=json.dumps(event['message']))
+        await self.send(text_data=json.dumps(event['message']))
+
+    @database_sync_to_async
+    def call_session_exists(self):
+        return CallSession.objects.filter(id=self.call_id).exists()
