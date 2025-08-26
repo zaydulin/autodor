be1530aa133721ff0c7941c797bb8ab708f2f3cb
vagiz <zaydulin.vagiz@gmail.com>
2025-08-26 15:44:14 +0300

    pdf_form.html

--- src/_project/settings/core_settings.py
+++ src/_project/settings/core_settings.py
@@ -88,7 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 
 ]
 
