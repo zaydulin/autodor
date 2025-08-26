418661828a4ec02e411d2f9206bb11904c06a868
zaydulin <55358024+zaydulin@users.noreply.github.com>
2025-08-26 16:45:30 +0300

    Update core_settings.py

--- src/_project/settings/core_settings.py
+++ src/_project/settings/core_settings.py
@@ -119,11 +119,22 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # для стандартной аутентификации
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '<YOUR_GOOGLE_CLIENT_ID>'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '<YOUR_GOOGLE_CLIENT_SECRET>'
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = '/accounts/login/'  # или любой другой URL
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+LOGIN_URL = 'useraccount:login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 
