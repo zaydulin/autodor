9a3f4363662cb31797ae6dedcabe04ee9645c7ec
akilo <sergeencowgame@yande.ru>
2025-09-08 13:45:07 +0300

    первичный суп

--- src/_project/urls.py
+++ src/_project/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', include('moderation.urls', namespace='moderation')),
     path('', include('useraccount.urls', namespace='useraccount')),
     path('', include('social_django.urls', namespace='social')),
+    path('', include('mobiledevaise.urls', namespace='moderation')),
 ]
 
 if settings.DEBUG:
