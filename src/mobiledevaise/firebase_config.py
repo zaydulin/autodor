02d9ffce5b3e61acd0e50f1e9e52556a7ce21fff
akilo <sergeencowgame@yande.ru>
2025-09-08 18:35:32 +0300

    первичный суп

--- src/mobiledevaise/firebase_config.py
+++ src/mobiledevaise/firebase_config.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials
 
 # Относительный путь к JSON-файлу
 current_dir = os.path.dirname(os.path.abspath(__file__))
-firebase_credentials_path = os.path.join(current_dir, "molnia-de89e-firebase-adminsdk-qjblk-733e766148.json")
+firebase_credentials_path = os.path.join(current_dir, "bahtrucks-firebase-adminsdk-fbsvc-5ac8009ced.json")
 
 # Инициализация Firebase Admin SDK
 if not firebase_admin._apps:
