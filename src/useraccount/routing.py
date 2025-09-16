03171a485db9fb1751d2bb0149ab398b340c7ffc
vagiz <zaydulin.vagiz@gmail.com>
2025-09-16 18:30:36 +0300

    123

--- src/useraccount/routing.py
+++ src/useraccount/routing.py
@@ -3,6 +3,9 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/blogs/(?P<blog_id>\d+)/chat/$', consumers.BlogChatConsumer.as_asgi()),
-    re_path(r'^ws/audio/(?P<user_id>[0-9a-f-]+)/$', consumers.AudioConsumer.as_asgi()),
+    re_path(
+        r'^ws/audio/(?P<user_id>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})/$',
+        consumers.AudioConsumer.as_asgi()
+    ),
 
 ]
