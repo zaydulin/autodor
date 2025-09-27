ceb4a347f7b7c9ed9299654ee900ca6476ee5c8f
akilo <sergeencowgame@yande.ru>
2025-09-27 19:10:00 +0300

    ё

--- src/moderation/routing.py
+++ src/moderation/routing.py
@@ -4,7 +4,7 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r"ws/call/(?P<call_id>\d+)/$", consumers.CallConsumer.as_asgi()),
     re_path(
-        r'^ws/chat/(?P<applications_id>[0-9a-fA-F-]{36})/$',
+        r'^ws/chat/(?P<applications_id>[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})/$',
         consumers.ChatConsumer.as_asgi()
     ),
     re_path(
