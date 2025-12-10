1dac2d9436bb479605ea0dab6a6ce39bfb5d52bb
root <root@bckzcuaweu.local>
2025-12-10 21:27:37 +0000

    fix: Исправлен конфиг nginx для ws, добавлен hostname для redis, обработка null current_driver

--- _config/nginx_config.sh
+++ _config/nginx_config.sh
@@ -19,7 +19,7 @@ server {
     server_name $DOMAIN_NAME www.$DOMAIN_NAME;
 
     location / {
-        proxy_pass http://0.0.0.0:8000;
+        proxy_pass http://127.0.0.1:8000;
         proxy_http_version 1.1;
         proxy_read_timeout 86400;
         proxy_redirect off;
@@ -34,7 +34,7 @@ server {
     }
 
     location /ws/ {
-        proxy_pass http://0.0.0.0:8001;
+        proxy_pass http://127.0.0.1:8000;
         proxy_http_version 1.1;
         proxy_read_timeout 86400;
         proxy_redirect off;
